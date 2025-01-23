26626cb20f7ab347aa5b8ae2ada54f434af30bec
ShashiSahani <shashisahani496@gmail.com>
2025-01-23 14:01:02 +0530

    update

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
 require("dotenv").config();
-const connectDB = require("./db/connect");
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const connectDB = require("./db/connect");
 const tasksRoutes = require("./routes/task");
-const PORT =process.env.PORT || 5489;
-
 const products_routes = require("./routes/products");
-const app = express();
 
-app.use(bodyParser.json());
-const dbUrl = process.env.DB_URL 
-;
+const app = express();
+const PORT = process.env.PORT || 5489;
+const dbUrl = process.env.DB_URL;
+
 if (!dbUrl) {
-  console.error("Error: Missing DB url env variable ");
-  process.exit("1");
+  console.error("Error: Missing DB URL environment variable.");
+  process.exit(1);
 }
+
+app.use(cors()); 
+app.use(bodyParser.json());
+
 app.use("/api/tasks", tasksRoutes);
+app.use("/api/products", products_routes);
 
 app.get("/", (req, res) => {
-  res.send("HI, I am live");
+  res.send("Hi, I am live!");
 });
-app.use("/api/products", products_routes);
+
 app.get('/ab*cd', (req, res) => {
-    res.send('ab*cd')
-  })
+  res.send('ab*cd');
+});
+
 app.listen(PORT, async () => {
   try {
     await connectDB(dbUrl);
